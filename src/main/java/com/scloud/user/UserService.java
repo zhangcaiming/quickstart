c163d24eb87854a83e29525ae20795bc72f1ca44
zcm <zhangcm3581@163.com>
2017-03-14 23:30:00 +0800

    去掉ServiceImpl

--- src/main/java/com/scloud/user/UserService.java
+++ src/main/java/com/scloud/user/UserService.java
@@ -1,27 +1,59 @@
 package com.scloud.user;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
-public interface UserService {
+@Service
+public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
 
-    Page<User> getUserList();
+    public Page<User> getUserList() {
+        return userRepository.findAll(new PageRequest(0,10));
+    }
 
-    User getUserById(Long id);
+    public User getUserById(Long id) {
+        return userRepository.findOne(id);
+    }
 
-    User update(User user);
+    @Transactional
+    public User update(User user) {
+        // user对象的id在数据库中存在，就更新
+        return userRepository.save(user);
+    }
 
-    void delete(Long id);
+    @Transactional
+    public void delete(Long id) {
+        userRepository.delete(id);
+    }
 
-    User add(User user);
+    @Transactional
+    public User add(User user) {
+        return userRepository.save(user);
+    }
 
-    void batchInsert(List<User> users);
+    // 批量新增
+    @Transactional
+    public void batchInsert(List<User> users) {
+        userRepository.save(users);
+    }
 
-    void batchUpdate(List<User> users);
+    // 批量更新
+    @Transactional
+    public void batchUpdate(List<User> users) {
+        userRepository.save(users);
+    }
 
-    void batchDel(List<User> users);
+    // 批量删除
+    @Transactional
+    public void batchDel(List<User> users) {
+        userRepository.deleteInBatch(users);
+    }
 
 }
